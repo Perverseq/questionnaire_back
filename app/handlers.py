e0f9dc8b1a83c09bbb53ab6ee0725e3e3e33500a
Perverseq <luchniknya@gmail.com>
2024-09-15 21:23:47 +0500

    Updated methods skeleton

--- app/handlers.py
+++ app/handlers.py
@@ -3,78 +3,101 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 
-from app.database import return_login, add_questions, found_description, get_details_active_questionnaire, save_answer, \
-    get_status_questionnaire, get_detail_closed_questionnaire
-from app.forms import UserLoginForm, SaveAnswer
+from app.database import user_with_login
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates/")
 
 
 # Проверка соответствия логина и пароля бд
-@router.post('/login')
-def login(userLoginForm: UserLoginForm):
-    user = return_login(userLoginForm.email, userLoginForm.password)
+@router.post('user/login')
+def login(email, password) -> JSONResponse:
+    """
+    Метод для авторизации пользователя.
+
+    :param email: почта зарегистрированного пользователя
+    :param password: пароль от учетной записи пользователя
+    :return: вся информация по пользователю
+    """
+    user = user_with_login(email, password)
 
     if user:
         result = {
             "id": user[0],
-            "full_name": user[3]
+            "full_name": user[1]
         }
         return JSONResponse(content=result)
     else:
         return JSONResponse(content={"result": "fail", "error": "Email/password invalid"})
 
 
-#Добавление новых вопросов в бд
-@router.post('/question/add')
-def add_question(question: str):
-    result = add_questions(question)
-    return JSONResponse(content=result)
+@router.get('list/questionnaires')
+def get_questionnaires(user_id: int, type: bool = None, max: int = 20) -> JSONResponse:
+    """
+    Метод для получения списка анкет с процентом завершенности
 
-
-#Возвращение всей информации по опросу
-@router.get('/get_questionnaires')
-def get_questionnaires(user_id: int):
-    questionnaires_list = found_description(user_id)
-# Если анкет нет, возвращается not found
-    if not questionnaires_list:
-        return JSONResponse(content={"result": "Not found"})
-# Если анкеты есть, возвращаются данные по этой анкете
-    result = dict()
-    result["questionnaires"] = questionnaires_list
-    return JSONResponse(content=result)
-
-
-# Метод, возвращающий людей, по которым нужно ответить в опросе и список вопросов
-@router.get('/get_detail_questionnaire')
-def get_detail_questionnaire(user_id: int, questionnaire_id: int):
-    status = get_status_questionnaire(questionnaire_id)
-    if not status:
-        return JSONResponse(content={"result": "Not found"})
-    print(status)
-
-    if status[0][0] == 'active':
-        result = get_details_active_questionnaire(user_id, questionnaire_id)
-        if result is False:
-            return JSONResponse(content={"result": "Not found"})
-        else:
-            return JSONResponse(content=result)
+    :param user_id: ID пользователя, для которого запрашивается список анкет.
+    :param type: Тип анкеты. С полученным ответом или нет.
+    :param max: Максимальное количество получаемых в ответе анкет.
+    :return: JSON с анкетами.
+    """
+    if type is None:
+        # Вернуть все анкеты
+        pass
+        return JSONResponse
+    elif type is True:
+        # Вернуть анкеты, по которым получен ответ
+        pass
+        return JSONResponse
+    elif type is False:
+        # Вернуть анкеты, по которым не получен ответ
+        pass
+        return JSONResponse
     else:
-        result = get_detail_closed_questionnaire(user_id, questionnaire_id)
-        return JSONResponse(content=result)
+        # Вернуть ошибку
+        pass
+        return JSONResponse
 
 
-# Метод сохранения ответов по анкете
-@router.post('/save_answers')
-def save_answers(answer: SaveAnswer):
-    if save_answer(answer):
-        return JSONResponse(content={"result": "saved successfully"})
-    else:
-        return JSONResponse(content={"result": "save failed"})
+@router.get('questionnaire/details')
+def get_questionnaire_details(questionnaire_id: int, user_id: int) -> JSONResponse:
+    """
+    Метод для получения анкеты для ответа. Список респондентов и вопросы.
+
+    :param questionnaire_id: ID анкеты.
+    :param user_id: ID пользователя.
+    :return: JSON со списком респондентом и списком вопросов.
+    """
+    # Вернуть список респондентов и список вопросов
+    pass
+    return JSONResponse
 
 
-# Изменение статуса анкеты
-# @router.get('/change_state')
-# def change_state(id_questionnaire: int):
-#     return change_state_questionnaire(id_questionnaire)
+@router.get('questionnaire/results')
+def get_questionnaire_results(questionnaire_id: int, user_id: int) -> JSONResponse:
+    """
+    Метод для получения результатов по анкете.
+
+    :param questionnaire_id: ID анкеты.
+    :param user_id: ID ползователя.
+    :return: JSON со списком вопросов, своими оценками на вопросы,
+    усредненными оценками других отвечающих, комментариями.
+    """
+    # Вернуть JSON
+    pass
+    return JSONResponse
+
+
+@router.post('questionnaire/save_answer')
+def post_questionnaire_save_answer(questionnaire_id: int, user_id: int, respondent_id: int, answers: dict) -> str:
+    """
+    Метод для сохранения ответа по анкете.
+
+    :param questionnaire_id: ID анкеты.
+    :param user_id: ID авторизованного пользователя.
+    :param respondent_id: ID респондента.
+    :param answers: Словарь с ответами и комментариями. {ID вопроса: [Оценка, Комментарий], ..}
+    :return: Статус успешно или нет.
+    """
+    pass
+    return ''
